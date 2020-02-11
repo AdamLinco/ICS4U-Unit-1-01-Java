d52c4bbc711c3aeb537a8017e05649174fc0c4fa
Ubuntu <ubuntu@ip-172-31-80-89.ec2.internal>
2020-02-11 18:18:53 +0000

    commit message

---
+++ Unit1HelloWorld.1.java
@@ -0,0 +1,17 @@
+/**
+ * Adam Linco
+ * 2020-02-11
+ * ICS4U-1 Unit 1-01
+ * 
+ */
+ 
+
+
+
+
+public class Unit1HelloWorld {
+	
+	public static void main(String []args) {
+      System.out.println("Hello, World!"); 
+   }
+}
